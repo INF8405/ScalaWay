1c5b706b4bcdedb8fb07c4d3c906cf43265d50ca
Guillaume Masse <masgui@gmail.com>
2013-01-17 20:20:26 -0500

    button pur pur pur

---
+++ src/main/scala/ca/polymtl/inf8405/MainActivity.scala
@@ -0,0 +1,35 @@
+package ca.polymtl.inf8405
+
+import _root_.model.Cat
+
+import android._
+import app.Activity
+import os.Bundle
+import view.View
+import view.View.OnClickListener
+
+class MainActivity extends Activity with TypedActivity
+{
+  override def onCreate(bundle: Bundle)
+  {
+    // monitor
+    // android:start-device
+    // attach remote debugger port 8600
+    // uncomment next two lines
+
+    // import android.os.Debug
+    // Debug.waitForDebugger()
+
+    super.onCreate(bundle)
+    setContentView(R.layout.main)
+
+    findView(TR.button).setOnClickListener( new OnClickListener         // TR is like R but with type information
+    {                                                                   // otherwise this would be like
+      def onClick(p1: View)                                             //
+      {                                                                 // findViewById(R.id.button_send).asInstanceOf[Button].
+        val cat = Cat("bob")                                            //  setOnClickListener(new View.OnClickListener() {
+        findView(TR.textview).setText( cat.pur )
+      }
+    })
+  }
+}
---
+++ src/main/scala/ca/polymtl/inf8405/model/Cat.scala
@@ -0,0 +1,6 @@
+package model
+
+case class Cat( name: String )
+{
+  def pur = name + " pur pur pur"
+}
