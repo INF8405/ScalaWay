8360e4424ffe35054085c5bc5f2f3ced674b3531
Guillaume Masse <masgui@gmail.com>
2013-01-29 23:54:16 -0500

    cant find a way to implement using JR model

--- src/main/scala/ca/polymtl/inf8405/MainActivity.scala
+++ src/main/scala/ca/polymtl/inf8405/MainActivity.scala
@@ -1,29 +1,29 @@
-//package ca.polymtl.inf8405
-//
-//// us
-//import controller.GameController
-//
-//// android
-//import android.app.Activity
-//import android.os.Bundle
-//
-//class MainActivity extends Activity with TypedActivity
-//{
-//  override def onCreate(bundle: Bundle)
-//  {
-//    // install jdk 1.6 ( yep Java 6 because of ProGuard )
-//    // start android-sdk/bin/monitor ( android device monitor )
-//    // android:start-device
-//    // attach remote debugger port 8600
-//    // uncomment next two lines
-//
-//    // import android.os.Debug
-//    // Debug.waitForDebugger()
-//
-//    super.onCreate(bundle)
-//    setContentView(R.layout.main)
-//
-//    val gameview = findView( TR.gameview )
-//    gameview.setOnTouchListener( new GameController( gameview ) )
-//  }
-//}
+package ca.polymtl.inf8405
+
+// us
+import controller.GameController
+
+// android
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity extends Activity with TypedActivity
+{
+  override def onCreate(bundle: Bundle)
+  {
+    // install jdk 1.6 ( yep Java 6 because of ProGuard )
+    // start android-sdk/bin/monitor ( android device monitor )
+    // android:start-device
+    // attach remote debugger port 8600
+    // uncomment next two lines
+
+    // import android.os.Debug
+    // Debug.waitForDebugger()
+
+    super.onCreate(bundle)
+    setContentView(R.layout.main)
+
+    val gameview = findView( TR.gameview )
+    gameview.setOnTouchListener( new GameController( gameview ) )
+  }
+}
--- src/main/scala/ca/polymtl/inf8405/model/Grid.scala
+++ src/main/scala/ca/polymtl/inf8405/model/Grid.scala
@@ -32,8 +32,39 @@ case class Grid( tokens: List[Token], links: List[Link], width: Int, height: Int
   def jtokens: java.util.List[Token] = tokens
   def jlinks: java.util.List[Link] = links
 
+  def find( coordinate: Coordinate ) =
+  {
+    val tokenPosition = tokens.find( _.position == coordinate )
+
+    if ( tokenPosition.isEmpty ) links.find( _.position == coordinate )
+    else tokenPosition
+  }
+
   def link( from: Coordinate, direction: Direction ): Grid =
   {
+    val to = from + direction
+
+    ( find( from ), find( to ) ) match {
+      case ( None, _ ) => this
+      case ( Some( linkable ), None ) =>
+      {
+        //links.find( _.from == from ).map( )
+
+        copy( links = Link( linkable, direction ) :: links )
+      }
+      case ( Some( linkable1 ), Some( linkable2 ) ) =>
+      {
+        if ( linkable1 sameColorAs linkable2 )
+        // self link ? => destroy and return this
+        // other link ? => destroy and link
+
+        // marker - marker ?
+
+        this
+      }
+      case _ => this
+    }
+
     ???
   }
 
@@ -44,7 +75,9 @@ case class Grid( tokens: List[Token], links: List[Link], width: Int, height: Int
 trait Linkable
 {
   def position: Coordinate
+  def sameColorAs( other: Linkable ) = other == this
 }
+
 case class Token( color: Color, coordinate: Coordinate ) extends Linkable
 {
   def position = coordinate
@@ -52,6 +85,11 @@ case class Token( color: Color, coordinate: Coordinate ) extends Linkable
 case class Link( from: Linkable, direction: Direction ) extends Linkable
 {
   def position = from.position + direction
+  override def sameColorAs( other: Linkable ) =
+  {
+    if( super.sameColorAs( other ) || other == from ) true
+    else from.sameColorAs( other )
+  }
 }
 
 sealed abstract class Direction
